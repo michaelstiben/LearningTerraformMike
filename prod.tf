e2a85b323aa69ffbbbb8e9ca48637e454cdb1d72
michaelstiben <michaelstiben@gmail.com>
2022-01-14 10:02:41 -0500

    rename first_code-tf to prod-tf and add default vpc

--- prod.tf
+++ prod.tf
@@ -3,7 +3,9 @@ provider "aws"{
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "tf_course_mike" {
+resource "aws_s3_bucket" "test_tf_course_mike" {
   bucket = "tf-course-mike-co"
   acl = "private"
 }
+
+resource "aws_default_vpc" "defaultvpc" {}
